12ff4bf32fbf6c2ae91c51e91f5a6a2021ed7221
zoekniskern <zxk3766@rit.edu>
2018-11-19 06:01:55 -0500

    Updates

---
+++ precache-manifest.b7a1b26575b619af031a72745eb1a74e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "acc0f34b8c21f7007ec0f37939361292",
+    "url": "/kniskern_dataviz_catalogue/static/media/CoffeesPerDay.acc0f34b.csv"
+  },
+  {
+    "revision": "64721e2bcdec9c20323a1a758df70f69",
+    "url": "/kniskern_dataviz_catalogue/static/media/CoffeeWake.64721e2b.csv"
+  },
+  {
+    "revision": "b2cedd24b20efdb955e4",
+    "url": "/kniskern_dataviz_catalogue/static/js/runtime~main.b2cedd24.js"
+  },
+  {
+    "revision": "51242127d5ca273860e1",
+    "url": "/kniskern_dataviz_catalogue/static/js/main.51242127.chunk.js"
+  },
+  {
+    "revision": "33685e307606d2cebafb",
+    "url": "/kniskern_dataviz_catalogue/static/js/1.33685e30.chunk.js"
+  },
+  {
+    "revision": "51242127d5ca273860e1",
+    "url": "/kniskern_dataviz_catalogue/static/css/main.e0d8d9f6.chunk.css"
+  },
+  {
+    "revision": "8c8a5975ff7fe2cd76c79987e37a546e",
+    "url": "/kniskern_dataviz_catalogue/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/kniskern_dataviz_catalogue/precache-manifest.b7a1b26575b619af031a72745eb1a74e.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/kniskern_dataviz_catalogue/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
